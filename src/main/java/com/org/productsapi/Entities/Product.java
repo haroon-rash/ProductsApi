990f4fe59b7c89c14163817e1552e13fd8a92c5e
Muhammad Haroon Ur Rasheed <haroonurrasheed1212@gmail.com>
2025-07-28 23:30:02 +0500

    Product Entities

---
+++ src/main/java/com/org/productsapi/Entities/Product.java
@@ -0,0 +1,37 @@
+package com.org.productsapi.Entities;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jdk.jfr.Name;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.UUID;
+@Data
+@NoArgsConstructor
+@Table("products")
+public class Product {
+
+    @Id
+    private UUID id;
+
+    @NotBlank(message = "Product name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @Min(value = 0, message = "Quantity must be 0 or more")
+    private int quantity;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    private double price;
+
+    @NotBlank(message = "Category is required")
+    private String categories;
+}
