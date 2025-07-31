d11e92fd886f6473d0a4d637a57c6f0f56881577
Muhammad Haroon Ur Rasheed <haroonurrasheed1212@gmail.com>
2025-07-31 18:31:35 +0500

    Product Repository

---
+++ src/main/java/com/org/productsapi/Repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.org.productsapi.Repository;
+
+
+import com.org.productsapi.Entities.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends ReactiveCrudRepository<Product, UUID> {
+
+    Product findAllById(UUID id);
+}
